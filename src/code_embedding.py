c538e84ce5d8ba427deb95afee81dbce00747c26
afmjoaa <mohimenul.joaa@bkash.com>
2023-07-03 10:53:36 +0600

    #1: Complete the code embedding class

--- src/code_embedding.py
+++ src/code_embedding.py
@@ -2,6 +2,20 @@ from transformers import AutoTokenizer, AutoModelForMaskedLM
 from transformers import RobertaConfig
 import functions_manager as fm
 import torch
+import torch.nn.functional as functional
+
+
+def getShape(embedding):
+    """Print and return the shape and length of an embedding
+
+    :param embedding: The embedding tensor whom shape and length is to be calculated
+    :return: Tuple as (shape, length)
+    """
+    shape = embedding.shape
+    length = len(embedding)
+    print(shape)
+    print(length)
+    return shape, length
 
 
 class CodeEmbedding:
@@ -11,13 +25,58 @@ class CodeEmbedding:
         self.model = AutoModelForMaskedLM.from_pretrained("microsoft/graphcodebert-base", config=self.config)
         self.function_manager = fm.FunctionManager()
 
+    def _getRawFunctionOutput(self, function_name: str):
+        """Calculate the output from the function input
+
+        :param function_name: Name of the function.
+        :return: The output which include hidden states and logits
+        """
+        function_ref = self.function_manager.getNameToReference().get(function_name)
+        func_raw_str = self.function_manager.getFunctionAsStringWithoutDocString(function_ref)
+        inputs = self.tokenizer(func_raw_str, return_tensors="pt")
+        outputs = self.model(**inputs)
+        return outputs
+
     def _getRawFunctionEmbedding(self, function_name: str):
-        pass
+        """Calculate the hidden states embedding for the function tokens
+
+        :param function_name: Name of the function.
+        :return: hidden states embedding of size [1 * n * 768]
+        """
+        function_ref = self.function_manager.getNameToReference().get(function_name)
+        func_raw_str = self.function_manager.getFunctionAsStringWithoutDocString(function_ref)
+
+        inputs = self.tokenizer(func_raw_str, return_tensors="pt")
+
+        outputs = self.model(**inputs)
+        hidden_states = outputs.hidden_states
+        return hidden_states
 
     def getPerfectFunctionEmbedding(self, function_name: str, max_length=300):
-        pass
+        """Reshape the last hidden state of the embedding into [max_length, 768] tensor
 
+        :param function_name: Name of the function.
+        :param max_length: Max length for token that is supported.
+        :return: Last hidden state embedding of size [n * 768]
+        """
+        hidden_states_embedding = self._getRawFunctionEmbedding(function_name)
 
-# hidden state
-# logits
-# shape
+        # Pad the tensor to the desired shape [300, 768]
+        padded_tensor = functional.pad(hidden_states_embedding[0],
+                                       (0, 0, 0, max_length - hidden_states_embedding[0].shape[1]))
+        reshaped_tensor = padded_tensor.squeeze()
+        return reshaped_tensor
+
+    def getLogits(self, function_name: str, max_length=300):
+        """Calculate the logits for the given function.
+
+        :param function_name: Name of the function.
+        :param max_length: Max length for token that is supported.
+        :return: Logits embedding.
+        """
+        output = self._getRawFunctionOutput(function_name)
+        logits = output.logits
+        # Pad the tensor to the desired shape [300, 768]
+        padded_tensor = functional.pad(logits, (0, 0, 0, max_length - logits.shape[1]))
+        reshaped_tensor = padded_tensor.squeeze()
+        return reshaped_tensor
